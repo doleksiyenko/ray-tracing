44a9d87a2ac38f655958cab6cdb21407263a45f1
Daniel Oleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2021-12-27 19:08:34 -0500

    Creating rays from the camera

--- scripts/RayTracingMaster.cs
+++ scripts/RayTracingMaster.cs
@@ -6,6 +6,11 @@ public class RayTracingMaster : MonoBehaviour
 {
     public ComputeShader RayTracingShader;
     private RenderTexture _target; // a RenderTexture is a texture that Unity creates and updates at runtime.
+    private Camera _camera;
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
     private void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
         /* 
@@ -13,6 +18,7 @@ public class RayTracingMaster : MonoBehaviour
         If we want to apply some material or processing effect, we read the pixels from the <src>, apply the effect, then copy the result
         to the <dest>.
         */
+        SetShaderParameters();
         Render(dest);
     }
     private void Render(RenderTexture dest)
@@ -22,7 +28,8 @@ public class RayTracingMaster : MonoBehaviour
 
         RayTracingShader.SetTexture(0, "Result", _target);
 
-        // calculate the threadGroups for the GPU. In our shader, we are using the default numthreads(8,8,1).
+        // calculate the threadGroups for the GPU. In our shader, we are using the default numthreads(8,8,1), so we set one thread group to a
+        // region of 8x8 pixels.
         int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
         int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
 
@@ -52,5 +59,12 @@ public class RayTracingMaster : MonoBehaviour
             _target.enableRandomWrite = true;
             _target.Create();
         }
+    }
+
+    private void SetShaderParameters()
+    {
+        // pass relevant matrices into the shader
+        RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
+        RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
     }
 }
